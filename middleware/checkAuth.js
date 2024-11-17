51669d700cfb7f939c815673ab608a6b9eff3f87
sawauchi ryota <ryota0922gks@gmail.com>
2024-11-17 12:20:37 +0900

    jwt token  authetication function

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,31 +3,22 @@ const config = require("../config");
 
 module.exports = (req, res, next) => {
 
-    //JWTを持っているか確認 -> リクエストヘッダの中のx-auth-tokenを確認
-    // ここがクッキーから取り出すところになるのか？
-    const token = req.header("x-auth-token");
+//   認証情報からjwtのトークンを取得
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1];
 
-    // const userVaild = false;
-    // const userVaild = true;
-
-    if (!token) {
-        res.status(400).json({
-            msg: "権限がありません",
-        });
-    } else {
-
-        try {
-
-            let user = JWT.verify(token, config.jwt.secret);
-            req.user = user.email;
-            // ミドルウェアを抜ける　※の処理に処理が移る
-            next();
-        } catch {
-            return res.status(400).json([
-                {
-                    msg: "tokenが一致しません",
-                },
-            ]);
-        }
+    if (!authHeader) {
+        return res.status(401).send('Access denied: No token provided');
     }
+
+    try {
+
+        const decoded = JWT.verify(token, config.jwt.secret);
+        req.user = decoded;
+        // ミドルウェアを抜ける　※の処理に処理が移る
+        next();
+    } catch (error) {
+        res.status(400).send("ユーザ情報が得られませんでした。");
+    }
+
 };
